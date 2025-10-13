43ffd88e5298433036c587d38038391727504d09
hassangazder <hassangazder3@gmail.com>
2025-10-13 16:07:27 +0400

    added multiple products

---
+++ week4/products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "a 25 pound bag of premium cat food",
+        "price": 2000,
+        "image": 'assets/images/cat1.jpg',
+        "availableInventory": 10,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "title": "Dog Toy, Squeaky Ball",
+        "description": "a fun squeaky ball for dogs",
+        "price": 599,
+        "image": 'assets/images/cat2.jpg',
+        "availableInventory": 25,
+        "rating": 4
+    },
+    {
+        "id": 1003,
+        "title": "Bird Cage, Large",
+        "description": "a spacious cage for large birds",
+        "price": 4999,
+        "image": 'assets/images/cat3.webp',
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "title": "Fish Tank, 20 Gallons",
+        "description": "a clear 20-gallon tank for aquatic life",
+        "price": 3500,
+        "image": 'assets/images/cat4.jpg',
+        "availableInventory": 8,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "title": "Hamster Wheel, Silent",
+        "description": "a silent running wheel for hamsters",
+        "price": 1200,
+        "image": 'assets/images/cat5.jpg',
+        "availableInventory": 15,
+        "rating": 3
+    }
+];
